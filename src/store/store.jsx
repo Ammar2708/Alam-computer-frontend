0476264d5668440d5d2c128e2e966c797efbd684
Ammar2708 <ammaralam1903@gmail.com>
2026-05-01 16:48:44 +0400

    Update service components and UI iconography

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./auth-slice";
-import AdminProductslice from "./admin/product-slice";
+import authReducer from "./auth-slice/index";
+import AdminProductslice from "./admin/product-slice/Index";
 import ShopProductSlice from "./shop/product-slice/index";
-import shoppingCartSlice from "./shop/cart-slice";
+import shoppingCartSlice from "./shop/cart-slice/index";
 import popupReducer from "./popup/popupSlice"; 
 import sliderReducer from "./slider/sliderSlice";
 import shopOrderSlice from "./order-slice/index";
