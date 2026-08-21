5170156f947baaa63028144b3cb557ce997d17da
Ammar Alam <ammaralam1903@gmail.com>
2026-08-21 21:59:52 +0500

    Update centralized business contact details

--- src/config/contact.js
+++ src/config/contact.js
@@ -1,14 +1,15 @@
 export const storeContact = {
-  phoneDisplay: "0528036944",
-  phoneHref: "tel:0528036944",
+  phoneDisplay: "+971 55 711 2599",
+  phoneHref: "tel:+971557112599",
   email: "alamcomputeruae@gmail.com",
   emailHref: "mailto:alamcomputeruae@gmail.com",
-  address: "Industrial Area 3, Sharjah, UAE",
-  shortAddress: "Industrial Area 3, Sharjah, UAE",
+  address: "J&P Signal, Industrial Area 3, Industrial Area, Sharjah",
+  shortAddress: "J&P Signal, Industrial Area 3, Sharjah",
   directionsHref:
-    "https://www.google.com/maps/dir/?api=1&destination=Intidhar%20Alam%20Computer%2C%20Industrial%20Area%203%2C%20Sharjah%2C%20UAE",
+    "directionsHref:
+  "https://www.google.com/maps/dir/?api=1&destination=Alam%20Computer%2C%20J%26P%20Signal%2C%20Industrial%20Area%203%2C%20Sharjah%2C%20UAE",",
   mapEmbedUrl:
-    "https://www.google.com/maps?q=Intidhar%20Alam%20Computer%2C%20Industrial%20Area%203%2C%20Sharjah%2C%20UAE&output=embed",
+  "https://www.google.com/maps?q=Alam%20Computer%2C%20J%26P%20Signal%2C%20Industrial%20Area%203%2C%20Sharjah%2C%20UAE&output=embed",
 };
 
 export const externalLinkProps = {
