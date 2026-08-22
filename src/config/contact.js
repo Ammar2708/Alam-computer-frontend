e146d6aa07030d82c46f781bd932dd175c502a82
Ammar Alam <ammaralam1903@gmail.com>
2026-08-22 23:37:19 +0500

    Fix contact configuration URLs

--- src/config/contact.js
+++ src/config/contact.js
@@ -3,16 +3,16 @@ export const storeContact = {
   phoneHref: "tel:+971557112599",
   email: "alamcomputeruae@gmail.com",
   emailHref: "mailto:alamcomputeruae@gmail.com",
-  address: "J&P Signal, Industrial Area 3, Industrial Area, Sharjah",
-  shortAddress: "J&P Signal, Industrial Area 3, Sharjah",
-  directionsHref:
-    "directionsHref:
-  "https://www.google.com/maps/dir/?api=1&destination=Alam%20Computer%2C%20J%26P%20Signal%2C%20Industrial%20Area%203%2C%20Sharjah%2C%20UAE",",
-  mapEmbedUrl:
-  "https://www.google.com/maps?q=Alam%20Computer%2C%20J%26P%20Signal%2C%20Industrial%20Area%203%2C%20Sharjah%2C%20UAE&output=embed",
-};
 
-export const externalLinkProps = {
-  target: "_blank",
-  rel: "noopener noreferrer",
+  address:
+    "J&P Signal, Industrial Area 3, Industrial Area, Sharjah, UAE",
+
+  shortAddress:
+    "J&P Signal, Industrial Area 3, Sharjah, UAE",
+
+  directionsHref:
+    "https://www.google.com/maps/dir/?api=1&destination=Alam%20Computer%2C%20J%26P%20Signal%2C%20Industrial%20Area%203%2C%20Sharjah%2C%20UAE",
+
+  mapEmbedUrl:
+    "https://www.google.com/maps?q=Alam%20Computer%2C%20J%26P%20Signal%2C%20Industrial%20Area%203%2C%20Sharjah%2C%20UAE&output=embed",
 };
